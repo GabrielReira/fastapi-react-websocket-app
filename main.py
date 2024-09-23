8cc6d95086cb5e86cdadaa7931425f0a36754278
Gabriel Reira <gabrielmfreira@gmail.com>
2024-09-23 20:06:11 +0200

    containerizing backend and other minor changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 
 
 app = FastAPI()
-DATABASE = 'data.json'
+DATABASE = 'data/data.json'
 
 class Item(BaseModel):
     name: str
@@ -14,28 +14,31 @@ class Item(BaseModel):
 async def root():
     return {'status': 'ok'}
 
+@app.get('/items/')
+async def get_items():
+    return read_db()
+
 @app.get('/items/{item_id}')
 async def get_item(item_id: int):
-    items = read_items_from_db()
+    items = read_db()
     if item_id < 0 or item_id >= len(items):
         return HTTPException(status_code=404, detail='Item not found')
     return {'item': items[item_id]}
 
 @app.post('/items/')
 async def create_item(item: Item):
-    items = read_items_from_db()
-    items.append(item.model_dump())
-    store_item_to_db(items)
+    db = read_db()
+    db.append(item.model_dump())
+    write_db(db)
     return {'result': item}
 
-def read_items_from_db():
+def read_db():
     try:
         with open(DATABASE, 'r') as file:
-            items = json.load(file)
+            return json.load(file)
     except FileNotFoundError:
-        items = []
-    return items
+        return []
 
-def store_item_to_db(data):
+def write_db(data):
     with open(DATABASE, 'w') as file:
         json.dump(data, file, indent=2)
