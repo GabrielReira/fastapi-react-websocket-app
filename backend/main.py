68a04f2253671c51150f6b633f568f5c5158fe46
Gabriel Reira <gabrielmfreira@gmail.com>
2024-09-30 19:08:09 +0200

    implementing websocket and dockerizing all app

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, WebSocket, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import json
@@ -20,14 +20,41 @@ class Product(BaseModel):
     price: float = None
     description: str = None
 
+# Websocket connection manager
+class WebsocketManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+    async def send_message(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+websocket_manager = WebsocketManager()
+
+# Websocket route for real-time updates of new products
+@app.websocket('/ws')
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket_manager.connect(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except:
+        websocket_manager.disconnect(websocket)
+    
+# Standard route
 @app.get('/')
 async def root():
     return {'status': 'ok'}
 
+# Route to list all products
 @app.get('/products/')
 async def get_products():
     return read_db()
 
+# Route to retrieve info about a specific product
 @app.get('/products/{product_id}')
 async def get_product(product_id: int):
     products = read_db()
@@ -35,11 +62,15 @@ async def get_product(product_id: int):
         return HTTPException(status_code=404, detail='Product not found')
     return {'product': products[product_id]}
 
+# Route to create a product
 @app.post('/products/')
 async def create_product(product: Product):
     db = read_db()
     db.append(product.model_dump())
     write_db(db)
+    # send message to all websockets clients connected
+    await websocket_manager.send_message(json.dumps(db))
+
     return {'result': product}
 
 def read_db():
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,26 @@ function App() {
   const [description, setDescription] = useState('');
 
   useEffect (() => {
+    // HTTP first request to load products
     fetch('http://localhost:8000/products/')
       .then((response) => response.json())
       .then((data) => setProducts(data))
       .catch((error) => console.error(error))
+
+    // Connect to WebSocket to retrieve newly products in real-time
+    const socket = new WebSocket('ws://localhost:8000/ws');
+    socket.onmessage = (event) => {
+      const updateProducts = JSON.parse(event.data);
+      setProducts(updateProducts);
+    }
+    socket.onerror = (error) => {
+      console.error(error);
+    }
+
+    return () => socket.close();
   }, []);
 
+  // HTTP request to create a new product
   const handleSubmit = (e) => {
     e.preventDefault();
     const newProduct = {
@@ -28,8 +42,7 @@ function App() {
       body: JSON.stringify(newProduct)
     })
       .then((response) => response.json())
-      .then((data) => {
-        setProducts([...products, data.result]);
+      .then(() => {
         setName('');
         setWeight('');
         setPrice('');
@@ -42,10 +55,10 @@ function App() {
       <h2>Create a Product</h2>
       <form onSubmit={handleSubmit}>
         <input type='text' value={name} placeholder='Name' onChange={e => setName(e.target.value)} />
-        <input type='text' value={weight} placeholder='Name' onChange={e => setWeight(e.target.value)} />
-        <input type='text' value={price} placeholder='Name' onChange={e => setPrice(e.target.value)} />
-        <input type='text' value={description} placeholder='Name' onChange={e => setDescription(e.target.value)} />
-        <button type='submit'>CREATE</button>
+        <input type='text' value={weight} placeholder='Weight' onChange={e => setWeight(e.target.value)} />
+        <input type='text' value={price} placeholder='Price' onChange={e => setPrice(e.target.value)} />
+        <input type='text' value={description} placeholder='Description' onChange={e => setDescription(e.target.value)} />
+        <button type='submit'>ADD PRODUCT</button>
       </form>
 
       <h2>Products List</h2>
