c3fb04f7ea102e65d4199deeb73fa70e772fc6eb
Gabriel Reira <gabrielmfreira@gmail.com>
2024-11-22 23:21:28 +0100

    enhance frontend with material ui and some stylization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
+import './App.css';
 import React, {useState, useEffect} from 'react';
 import { fetchProducts, createProduct, subscribeToWebsocket } from './services/api';
+import { Container, Box, Grid2, TextField, Button, Card, CardContent, Grid } from '@mui/material';
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -39,25 +41,74 @@ function App() {
   }
 
   return (
-    <div>
-      <h2>Create a Product</h2>
-      <form onSubmit={handleSubmit}>
-        <input type='text' value={name} placeholder='Name' onChange={e => setName(e.target.value)} />
-        <input type='text' value={weight} placeholder='Weight' onChange={e => setWeight(e.target.value)} />
-        <input type='text' value={price} placeholder='Price' onChange={e => setPrice(e.target.value)} />
-        <input type='text' value={description} placeholder='Description' onChange={e => setDescription(e.target.value)} />
-        <button type='submit'>ADD PRODUCT</button>
-      </form>
+    <Container>
+      <Box className='createProductCard' component='form' onSubmit={handleSubmit}>
+        <h2>Create a Product</h2>
+        <Grid2 container spacing={1}>
+          <Grid2 item size={4}>
+            <TextField
+              label='Name'
+              value={name}
+              onChange={e => setName(e.target.value)}
+              required
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 item size={4}>
+            <TextField
+              label='Price (€)'
+              value={price}
+              onChange={e => setPrice(e.target.value)}
+              type='number'
+              required
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 item size={4}>
+            <TextField
+              label='Weight (kg)'
+              value={weight}
+              onChange={e => setWeight(e.target.value)}
+              type='number'
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 item size={12}>
+            <TextField
+              label='Description'
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+              multiline
+              rows={2}
+              fullWidth
+            />
+          </Grid2>
+        </Grid2>
+        <Grid2 className='addProductBtn'>
+          <Button type='submit' variant='contained' fullWidth>
+            Add product
+          </Button>
+        </Grid2>
+      </Box>
 
-      <h2>Products List</h2>
-      <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            {product.name} - {product.weight}kg - €{product.price} - {product.description}
-          </li>
-        ))}
-      </ul>
-    </div>
+      <Box className='infoProductCard'>
+        <h2>Products List</h2>
+        <Grid2 container spacing={1}>
+            {products.map((product, index) => (
+              <Grid2 item size={4} key={index}>
+                <Card>
+                  <CardContent>
+                    {product.name} <br/>
+                    €{product.price} <br/>
+                    {product.weight}kg <br/>
+                    {product.description}
+                  </CardContent>
+                </Card>
+              </Grid2>
+            ))}
+        </Grid2>
+      </Box>
+    </Container>
   );
 }
 
